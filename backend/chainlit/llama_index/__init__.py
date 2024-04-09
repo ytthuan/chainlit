379fe9398c92bf4a418a987192426ca1f4629c59
Leo Yen <yenthuan@gmail.com>
2024-04-09 11:05:00 +0700

    cmmit

--- backend/chainlit/llama_index/__init__.py
+++ backend/chainlit/llama_index/__init__.py
@@ -1,6 +1,6 @@
 from chainlit.utils import check_module_version
 
-if not check_module_version("llama_index.core", "0.10.15"):
+if not check_module_version("llama_index.core", "0.10.27"):
     raise ValueError(
         "Expected LlamaIndex version >= 0.10.15. Run `pip install llama_index --upgrade`"
     )
